e7421ff16166accb50ac6db3237856687595979d
Patrick Nicezi <patrickk0806@gmail.com>
2024-09-13 14:11:59 -0300

    refactor: imports order

--- src/modules/course/course.module.ts
+++ src/modules/course/course.module.ts
@@ -4,10 +4,10 @@ import { CourseRepository } from '@shared/repositories';
 
 import { CreateCourseController } from './contexts/createCourse/createCourse.controller';
 import { CreateCourseService } from './contexts/createCourse/createCourse.service';
-import { UpdateCourseController } from './contexts/updateCouse/updateCourse.controller';
-import { UpdateCourseService } from './contexts/updateCouse/updateCourse.service';
 import { FindCourseByIdController } from './contexts/findCourseById/findCourseById.controller';
 import { FindCourseByIdService } from './contexts/findCourseById/findCourseById.service';
+import { UpdateCourseController } from './contexts/updateCouse/updateCourse.controller';
+import { UpdateCourseService } from './contexts/updateCouse/updateCourse.service';
 
 @Module({
   imports: [],
